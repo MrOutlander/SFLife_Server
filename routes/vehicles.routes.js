4fbec79aaccf79be8ff08003becc2ecb171d64f7
MrOutlander <122881426+MrOutlander@users.noreply.github.com>
2024-01-27 21:18:23 +0000

    changed order in the router

--- routes/vehicles.routes.js
+++ routes/vehicles.routes.js
@@ -12,7 +12,10 @@ import {
 } from '../controllers/vehicles.controller.js'
 
 const router = express.Router();
+// MOBILE
+router.route('/mobile/').get(getAllVehiclesMobile);
 
+// DESKTOP
 router.route('/').get(getAllVehicles);
 router.route('/:id').get(getVehicleById);
 router.route('/').post(createVehicle);
@@ -20,7 +23,5 @@ router.route('/:id').patch(editVehicle);
 router.route('/:id').delete(deleteVehicle);
 
 
-// MOBILE
-router.route('/mobile').get(getAllVehiclesMobile);
 
 export default router;
